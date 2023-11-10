19f03021a7172d1ea67fe7b1a68f89d153077d6f
LaurentSch <48076819+LaurentSch@users.noreply.github.com>
2023-11-10 07:28:29 +0100

    Add right to left energy trace simulation

--- src/rsa_simple_power_attack-spa.py
+++ src/rsa_simple_power_attack-spa.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 
 # This is the implementation of the exponent function from the fast_exponentiation.py file
+# aka my left to right exponentiation
 def fast_exponent(base, exp, mod):
     start_time = time.time()
     power_trace = []
@@ -27,14 +28,45 @@ def fast_exponent(base, exp, mod):
     return step, power_trace
 
 
-def plot_power_trace(power_trace):
+# handout right-to-left exponentiation
+def fast_exp(a, n, m):
+    start_time = time.time()
+    power_trace = []
+
+    x = a  # x = a^20
+    y = a if n & 1 else 1  # y = a^n0
+    while n > 0:
+        n >>= 1  # Right shift n by 1 (equivalent to n //= 2)
+        x = (x ** 2) % m  # x = a^(2i-1) → x = a^(2i)
+        time.sleep(0.01)
+        power_trace.append((time.time() - start_time, 5 + random.uniform(0.1, 0.5)))
+
+        if n & 1:
+            y = x if y == 1 else (y * x) % m  # y = a^(ni-1) → y = a^ni
+            time.sleep(0.01)
+            power_trace.append((time.time() - start_time, 8 + random.uniform(0.1, 0.5)))
+    return y, power_trace
+
+
+def plot_side_by_side(power_trace1, power_trace2):
+    plt.subplot(1, 2, 1)
+    plot_power_trace(power_trace1, 'Left-to-Right')
+
+    plt.subplot(1, 2, 2)
+    plot_power_trace(power_trace2, 'Right-to-Left')
+
+    plt.show()
+
+
+def plot_power_trace(power_trace, title):
     time_values, y_values = zip(*power_trace)
     plt.plot(time_values, y_values, marker='o', linestyle='-')
     plt.xlabel('Time')
     plt.ylabel('Power Consumption')
-    plt.show()
+    plt.title(title)
 
 
+# works only for left to right exponentiation
 def translate_trace(power_trace):
     private_exponent = "1"
     # Boolean variable that tracks if the previous step was the basic step for every bit in the exponent
@@ -56,4 +88,5 @@ if __name__ == "__main__":
     result, trace = fast_exponent(4288743, 8234214, 43)
     print(bin(8234214)[2:])
     print(translate_trace(trace))
-    plot_power_trace(trace)
+    result2, trace2 = fast_exp(4288743, 8234214, 43)
+    plot_side_by_side(trace, trace2)
